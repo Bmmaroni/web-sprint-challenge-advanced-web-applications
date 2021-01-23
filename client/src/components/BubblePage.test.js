33da67af5f62674cde22a04f27f55a04c985cec8
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-22 19:58:01 -0700

    updating test code

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { render, screen, await } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { act } from 'react-dom/test-utils';
 import BubblePage from "./BubblePage";
 
 import { getColors as mockGetColors } from './BubblePage';
-jest.mock('http://localhost:5000/api/colors')
+jest.mock('./BubblePage');
 
 const colorData = {
+  data: [
+    {
+      color: 'aliceblue',
+      code: {
+        hex: '#f0f8ff'
+      },
+      id: 1
+    },
 
+    {
+      color: 'limegreen',
+      code: {
+        hex: '#99ddbc'
+      },
+      id: 2
+    }
+  ]
 }
 
 test("Fetches data and renders the bubbles", async () => {
@@ -16,9 +32,13 @@ test("Fetches data and renders the bubbles", async () => {
   const { rerender } = render(<BubblePage />);
   screen.debug();
   await act(async () => {
-    await rerender(<BubblePage />);
+    await rerender(<BubblePage />, <Bubbles />);
     screen.debug();
   });
 
-  
+  const title = screen.getByRole('paragraph');
+
+  await waitFor(() => {
+    expect(title).toHaveTextContent(/bubbles/i)
+  });
 });
